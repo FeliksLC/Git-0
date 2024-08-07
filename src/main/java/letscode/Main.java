4724bad69c00cae2652b50d564a0fa99479a062a
Omar Langustas Crabos <OmarLC@yandex.ru>
2024-08-08 00:27:59 +0300

    Добавил сканер с выводом числа, перевел стоку приветствия в String переменную

--- src/main/java/letscode/Main.java
+++ src/main/java/letscode/Main.java
@@ -1,7 +1,13 @@
 package letscode;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        String str = "Hello Git!";
+        System.out.println(str);
+        Scanner scanQ = new Scanner(System.in);
+        int x = scanQ.nextInt();
+        System.out.println(str + " egen " + x);
     }
 }
